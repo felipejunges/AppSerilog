1e22fe3987f4afd8733c8440eb2987ea6d7c9e0a
Felipe Amadeu Junges <felipe.junges@cwi.com.br>
2019-07-01 18:00:30 -0300

    :tada: First commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+using System;
+using System.IO;
+
+namespace AppSerilog
+{
+    class Program
+    {
+        public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+            .Build();
+
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(Configuration)
+                .CreateLogger();
+
+            Log.Information("Teste");
+            Log.Logger.Information("Teste2");
+            Log.Logger.Warning("Teste3");
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
